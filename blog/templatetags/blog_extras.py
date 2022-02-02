059515715aa151129996e63d7614f5f96efc1b79
Md. Zahan <mszahan@hotmail.com>
2022-02-02 10:25:02 +0000

    Finish custom filters

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,42 @@
+from django import template
+from django.contrib.auth import get_user_model
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+from django.utils.html import format_html
+
+register = template.Library()
+
+user_model = get_user_model()
+
+@register.filter
+def author_details(author, current_user=None):
+  if not isinstance(author, user_model):
+  # the isinstance medthode checks whether....
+  # it is the right...
+  # type of objects or not.
+  # return empty string as safe default
+    return ""
+
+  if author == current_user:
+    return format_html("<strong>me</strong>")
+
+  if author.first_name and author.last_name:
+    #name = escape(f"{author.first_name} {author.last_name}") 
+    name = f"{author.first_name} {author.last_name}" 
+  else:
+    #name = escape(f"{author.username}")
+    name = escape(f"{author.username}")
+  
+  if author.email:
+    # following line is valid but ....
+    # if you forgot to add escape it will harm
+    #email = escape(author.email)
+    #prefix = f'<a href="mailto:{email}">'
+    prefix = format_html('<a href="mailto:{}">', author.email)
+    suffix = format_html("</a>")
+  else:
+    prefix = ""
+    suffix = ""
+  
+  #return mark_safe(f"{prefix} {name} {suffix}")
+  return format_html('{} {} {}', prefix, name, suffix)
